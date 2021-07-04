e200fc3ca3e04ccfafc48b38b687c5ab06d46c6a
Dylan Howard <dylanhoward0424@outlook.com>
2021-07-04 13:01:52 -0500

    added function listItems

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,14 +3,21 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 let basket = [];
 
-function addItem( item ){
+function addItem( item ) {
   basket.push(item);
   return true;
 }
-
 console.log('testing addItem:', addItem('Mac & Cheese'));
 console.log('testing addItem:', addItem('Soy Sauce'));
 console.log('testing addItem:', addItem('Butter'));
 console.log('testing addItem:', addItem('Chicken Thighs'));
 console.log('testing addItem:', addItem('Rice'));
 console.log('testing addItem:', addItem('Ginger'));
+
+function listItems() {
+  console.log('here are the items in your cart:');
+    for (i = 0; i < basket.length; i++){
+      console.log(basket[i]);
+    }
+}
+console.log(listItems());
